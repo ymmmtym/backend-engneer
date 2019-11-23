1b6ba54eb529f71f447176802737d497c037bdce
ymmmtym <h.yukihisa.378@gmail.com>
2019-11-23 16:41:49 +0900

    mv samplecode to gist

--- python/diveintoexam/dic.py
+++
@@ -1,7 +0,0 @@
-dic = [
-    ['Noro', 'Nakao', 'Miyaoka'],
-    ['Kimura', 'Miyashita', 'Shibata'],
-    ['Matsumoto', 'Tanaka', 'Ivan'],
-]
-
-print(list(zip(*dic)))
--- python/diveintoexam/lambda_sort.py
+++
@@ -1,4 +0,0 @@
-dic = [(1, 'Noro'), (2, 'Nakao'), (3, 'Miyaoka'), (4, 'Kimura')]
-dic.sort(key=lambda dic: dic[1])
-
-print(dic)
