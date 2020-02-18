5e33bdd1e6080596549271c70fc62e02e838db0f
ymmmtym <h.yukihisa.378@gmail.com>
2020-02-18 22:17:27 +0900

    fixed php scripts for #13

--- scripts/backend/php/array2json.php
+++ scripts/backend/php/array2json.php
@@ -1,26 +1,29 @@
 <?php
-
-// 連想配列($array)
+// array sample
 $array = array(
-  "name" => "yumemo" ,
+  "name" => "ymmmtym" ,
   "gender" => "unknown" ,
   "favorite" => array(
-    "music" => "音楽" ,
+    "music" => "any" ,
     "published" => "2014-06-10" ,
     "url" => "https://hogehoge.jp/" ,
   ),
 );
 
-$json1 = json_encode( $array ) ;
-echo "連想配列をJSONに変換(エンコード)する\n$json1\n\n";
+// encode array to json
+$json = json_encode( $array , JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES ) ;
 
-$json2 = json_encode( $array , JSON_UNESCAPED_UNICODE ) ;
-echo "unicodeをescapeしない\n$json2\n\n";
-
-$json3 = json_encode( $array , JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES ) ;
-echo "\\をescapeしない\n$json3\n\n";
-
-$json4 = json_encode( $array , JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES ) ;
-echo "改行とインデントをつける\n$json4\n\n";
+echo "$json\n\n";
+/* output
+{
+  "name": "ymmmtym",
+  "gender": "unknown",
+  "favorite": {
+      "music": "any",
+      "published": "2014-06-10",
+      "url": "https://hogehoge.jp/"
+  }
+}
+*/
 
 ?>
--- scripts/backend/php/scraping.php
+++ scripts/backend/php/scraping.php
@@ -1,6 +1,7 @@
 <?php
 
-  $src = file('http://blog.higty.xyz/archives/530/');
+  $url = $argv[1];
+  $src = file($url);
   $implodeSource = implode( "", $src ) ;
   $trimSource = preg_replace( "/(n|r|t)/", "", $implodeSource );
   preg_match_all( "/<h[1-5]>(.*?)<\/h[1-5]>/i", $trimSource, $matches ) ;
